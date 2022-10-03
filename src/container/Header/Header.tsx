bf920a82523935df435013db8b3ceba30ab6f5eb
Klaudiuszu <klaudiuszpupiec.kontakt@gmail.com>
2022-10-03 16:25:01 +0200

    finish header

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -5,10 +5,9 @@ import Particles from "react-tsparticles";
 import { motion, useMotionValue, useTransform } from "framer-motion";
 
 import FilterPokemon from "../../components/FilterProduct/FilterPokemon";
-
 import FilterCards from "../../components/FilterProduct/FilterCards";
-
 import { images } from "../../utils";
+
 import "./Header.scss";
 
 function Header() {
@@ -18,15 +17,21 @@ function Header() {
   const [detailsPokemon, setDetailsPokemon] = useState<any[]>([]);
   const [filterElement, setFilterElement] = useState("");
 
+  /* //------------------------------------------ */
+  // 1. Tworzymy nowy useState dla przefiltowanych pokemonów. Jeśli będziesz filtrował oryginalną listę to za drugim filtorwaniem przefiltrujesz listę, która jest już raz przefitrowana, a na pewno nie o to chodzi.
+  // nie szukam typu dla listy pokemonów, bo nie o to chodzi w zadaniu, ale ogólnie to trzeba poszukać :)
+  const [filteredPokemonsByType, setFilteredPokemonsByType] = useState<any[]>([]);
+
+  // console.log("filterElement: ", filterElement);
+  // console.log("detailsPokemon: ", detailsPokemon);
+
   useEffect(() => {
     const fetchPokemons = async () => {
       let response: any;
       let detailsResponse: any;
 
       try {
-        response = await axios.get(
-          `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
-        );
+        response = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`);
 
         setPokemonLIst(response.data.results);
       } catch (err) {
@@ -50,18 +55,23 @@ function Header() {
   }, [limit, offset]);
 
   //------------------------------------------
-  //znalazlem rozwiązanie na filtrowanie elementów, stan filtered Pokemons powinien wyświetlać mi elementy przefiltrowane po kliknięciu natomiast wlaśnie nie wiem jak przekazać ten stan z filterPokemon. Wcześniej miałem to wszystko w jednym komponencie ale to filtrowanie raz działalo raz nie działało
-  const filterPokemons = (event :any) => {
-      const filteredPokemons = detailsPokemon.filter(
-        pokemon => (`${pokemon.types[0].type.name}`.toLowerCase().includes(filterElement))
-      );
-      setDetailsPokemon(filteredPokemons)
-      console.log(detailsPokemon)
-    };
+  // znalazlem rozwiązanie na filtrowanie elementów, stan filtered Pokemons powinien wyświetlać mi elementy przefiltrowane po kliknięciu natomiast wlaśnie nie wiem jak przekazać ten stan z filterPokemon. Wcześniej miałem to wszystko w jednym komponencie ale to filtrowanie raz działalo raz nie działało
+  // const filterPokemons = (event: any) => {
+  //   const filteredPokemons = detailsPokemon.filter((pokemon) => `${pokemon.types[0].type.name}`.toLowerCase().includes(filterElement));
+  //   setDetailsPokemon(filteredPokemons);
+  //   // console.log(detailsPokemon);
+  // };
 
-    console.log(filterPokemons)
+  /* //------------------------------------------ */
+  // 2. Mała korekta w funkcji
+  // Pokemon zawiera tablicę types. Pojedyńczy type to objekt zawierający name. To name to nasz strong, który chcemy znaleźć. Wcześniej chciałeś wyszukać tylko pierwszy element tablicy types czyli types[0], a pokemon może mieć kilka typów, wiec trzeba sprawdzić wszystkie.
+  const filterPokemons = (event: any) => {
+    const result = detailsPokemon.filter((pokemon) => pokemon.types.some((type: any) => type.type.name === filterElement));
+    setFilteredPokemonsByType(result);
+  };
 
-
+  console.log("filteredPokemonsByType: ", filteredPokemonsByType);
+  // console.log(filterPokemons);
 
   //------------------------------------------
 
@@ -70,23 +80,25 @@ function Header() {
       <div className="app__Header-content">
         <div className="app__Header-list">
           <h2>Filter by type:</h2>
-          <FilterPokemon 
-          setFilterPokemons= {setFilterElement}
-          
-          />
+          <FilterPokemon setFilterPokemons={setFilterElement} />
 
           <div className="button-fetch">
-            <button>See pokemons!</button>
+            {/* 3. Dodanie zdarzenia onClick i wywołanie funkcji na zdarzenie */}
+            <button onClick={filterPokemons}>See pokemons!</button>
           </div>
         </div>
 
         <div className="app__Header-display">
-          {detailsPokemon.map((pokemon, index) => (
-            <FilterCards 
-            detailsPokemon={pokemon} 
-            key={index} 
-            />
+          {/* {detailsPokemon.map((pokemon, index) => (
+            <FilterCards detailsPokemon={pokemon} key={index} />
+          ))} */}
+
+          {/* //------------------------------------------ */}
+          {/* 4. Sprawdzenie czy są wyfiltorowane pokemony => jesli są to wyświetlamy wyfitrowane pokemony / jesli nie to wyświetlamy oryginalną listę  */}
+          {((filteredPokemonsByType.length && filteredPokemonsByType) || detailsPokemon).map((pokemon, index) => (
+            <FilterCards detailsPokemon={pokemon} key={index} />
           ))}
+
           {/* {detailsPokemon.map((item) => (
             <div key={item.id} className="app__Header-card">
               <div className="app__Header-img">
